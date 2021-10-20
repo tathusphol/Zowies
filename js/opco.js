36e711aafa97d181278f5986234e19003001ffb7
tathusphol <73683391+tathusphol@users.noreply.github.com>
2021-10-20 11:50:56 +0700

    fixed all body

---
+++ js/opco.js
@@ -0,0 +1,12 @@
+function openn(num){
+    document.getElementById(`eye${num}`).classList.toggle('hidden');
+    document.getElementById(`pickcart${num}`).classList.toggle('hidden');
+    document.getElementById(`pickcart${num}`).classList.toggle('animatetextcart');
+    document.getElementById(`price${num}`).classList.toggle('hidden');
+}
+function closee(num){
+    document.getElementById(`eye${num}`).classList.toggle('hidden');
+    document.getElementById(`pickcart${num}`).classList.toggle('hidden');
+    document.getElementById(`pickcart${num}`).classList.toggle('animatetextcart');
+    document.getElementById(`price${num}`).classList.toggle('hidden');
+}

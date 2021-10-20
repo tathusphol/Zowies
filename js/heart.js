a0d851634cc1e5aff3eb32d7d87e15fd716a97dc
tathusphol <73683391+tathusphol@users.noreply.github.com>
2021-10-20 15:05:07 +0700

    fixed index.html

--- js/heart.js
+++
@@ -1,7 +0,0 @@
-function heart(item) {
-    item.setAttribute('src', 'https://cdn.discordapp.com/attachments/896721421621919754/900253941663682640/heart-red.png');
-}
-
-function unheart(item) {
-    item.setAttribute('src', 'https://cdn.discordapp.com/attachments/896721421621919754/900253943295250442/heart.png');
-}

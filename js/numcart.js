596b2ff5534f09637ac5ecc96528c7b3020830aa
tathusphol <73683391+tathusphol@users.noreply.github.com>
2021-11-08 14:45:48 +0700

    fixed index

--- js/numcart.js
+++ js/numcart.js
@@ -17,3 +17,30 @@ if (numitem == undefined) {
 else {
     var numcart = document.getElementById('numcart').innerHTML = numitem;
 }
+var check = 1;
+document.getElementById('button').onclick = function () {
+    document.getElementById("sidebar").classList.toggle("animate-open");
+    document.getElementById("sidebar").classList.toggle("right-0");
+    document.getElementById("sidebar").classList.toggle("hidebar");
+    document.body.classList.toggle('overflow-hidden');
+    setTimeout(() => {
+        document.getElementById('bg-sidebar').classList.toggle('hidden');
+    }, 200);
+    if (check != 1) {
+        document.getElementById("sidebar").classList.toggle("animate-close");
+    }
+    check += 1;
+}
+document.getElementById('button1').onclick = function () {
+    document.getElementById("sidebar").classList.toggle("animate-open");
+    document.getElementById("sidebar").classList.toggle("right-0");
+    document.getElementById("sidebar").classList.toggle("hidebar");
+    document.body.classList.toggle('overflow-hidden');
+    setTimeout(() => {
+        document.getElementById('bg-sidebar').classList.toggle('hidden');
+    }, 200);
+    if (check != 1) {
+        document.getElementById("sidebar").classList.toggle("animate-close");
+    }
+    check += 1;
+}
